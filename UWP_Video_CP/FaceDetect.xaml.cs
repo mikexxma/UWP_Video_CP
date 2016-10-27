53d9aa05d46420d5c7dfa3b9554ab83556daf480
mike <mike@V-DOXIE-W10>
2016-10-27 18:10:50 +0800

    get faces

--- UWP_Video_CP/FaceDetect.xaml.cs
+++ UWP_Video_CP/FaceDetect.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Graphics.Imaging;
@@ -24,22 +25,24 @@ using Windows.UI.Xaml.Shapes;
 
 namespace UWP_Video_CP
 {
-    /// <summary>
+   
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class FaceDetect : Page
     {
+        StorageFile photoFile;
+        List<WriteableBitmap> facelist;
+        /// <summary>
         public FaceDetect()
         {
+            facelist = new List<WriteableBitmap>();
             this.InitializeComponent();
         }
 
+       
 
-        private  void SetupVisualization(WriteableBitmap displaySource, IList<DetectedFace> foundFaces)
+        private async void SetupVisualization(WriteableBitmap displaySource, IList<DetectedFace> foundFaces)
         {
-
-
-
             // Set the source of the Image control
 
             ImageBrush brush = new ImageBrush();
@@ -67,7 +70,33 @@ namespace UWP_Video_CP
                     box.StrokeThickness = 2.0;
                     box.Margin = new Thickness((uint)(face.FaceBox.X / widthScale), (uint)(face.FaceBox.Y / heightScale), 0, 0);
 
+                    Point point = new Point();
+                    point.X = face.FaceBox.X;
+                    point.Y = face.FaceBox.Y;
+                    Size size = new Size();
+                    size.Height = face.FaceBox.Height;
+                    size.Width = face.FaceBox.Width;
+                    facelist.Add(await GetCroppedBitmapAsync(photoFile, point, size, 1));
                     this.detectedResult.Children.Add(box);
+                    //for (int i = 0; i < 6; i++)
+                    //{
+                    //    BitmapImage bitmapImage = new BitmapImage();
+                    //    InMemoryRandomAccessStream randomAccessStream = new InMemoryRandomAccessStream();
+                    //    Stream pixelStream = facelist[i].PixelBuffer.AsStream();
+                    //    BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, randomAccessStream);
+
+                    //    byte[] pixels = new byte[pixelStream.Length];
+                    //    await pixelStream.ReadAsync(pixels, 0, pixels.Length);
+                    //    // Save the image file with jpg extension 
+                    //    encoder.SetPixelData(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Ignore, (uint)facelist[i].PixelWidth, (uint)facelist[i].PixelHeight, 96.0, 96.0, pixels);
+                    //    await encoder.FlushAsync();
+
+                    //    randomAccessStream.Seek(0);
+              
+                    //    bitmapImage.SetSource(randomAccessStream);
+                    //    Image img = (Image)this.FindName("face" + i);
+                    //    img.Source = bitmapImage;
+                    //}
                 }
             }
 
@@ -104,7 +133,7 @@ namespace UWP_Video_CP
                 photoPicker.FileTypeFilter.Add(".png");
                 photoPicker.FileTypeFilter.Add(".bmp");
 
-                StorageFile photoFile = await photoPicker.PickSingleFileAsync();
+                photoFile = await photoPicker.PickSingleFileAsync();
                 if (photoFile == null)
                 {
                     return;
@@ -179,6 +208,73 @@ namespace UWP_Video_CP
         private void ClearVisualization()
         {
             this.detectedResult.Children.Clear();
+        }
+
+
+       
+
+
+
+        async public static Task<WriteableBitmap> GetCroppedBitmapAsync(StorageFile originalImageFile, Point startPoint, Size corpSize, double scale)
+        {
+            if (double.IsNaN(scale) || double.IsInfinity(scale))
+            {
+                scale = 1;
+            }
+
+            // Convert start point and size to integer.
+            uint startPointX = (uint)Math.Floor(startPoint.X * scale);
+            uint startPointY = (uint)Math.Floor(startPoint.Y * scale);
+            uint height = (uint)Math.Floor(corpSize.Height * scale);
+            uint width = (uint)Math.Floor(corpSize.Width * scale);
+            using (IRandomAccessStream stream = await originalImageFile.OpenReadAsync())
+            {
+                // Create a decoder from the stream. With the decoder, we can get 
+                // the properties of the image.
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+                // The scaledSize of original image
+                uint scaledWidth = (uint)Math.Floor(decoder.PixelWidth * scale);
+                uint scaledHeight = (uint)Math.Floor(decoder.PixelHeight * scale);                        
+                // Refine the start point and the size. 
+                if (startPointX + width > scaledWidth)
+                {
+                    startPointX = scaledWidth - width;
+                }            
+                if (startPointY + height > scaledHeight)
+                {
+                    startPointY = scaledHeight - height;
+                }
+                // Get the cropped pixels.
+                byte[] pixels = await GetPixelData(decoder, startPointX, startPointY, width, height,scaledWidth, scaledHeight);
+                // Stream the bytes into a WriteableBitmap
+                WriteableBitmap cropBmp = new WriteableBitmap((int)width, (int)height);
+                Stream pixStream = cropBmp.PixelBuffer.AsStream();
+                pixStream.Write(pixels, 0, (int)(width * height * 4));
+                return cropBmp;
+            }
+        }
+
+        async static private Task<byte[]> GetPixelData(BitmapDecoder decoder, uint startPointX, uint startPointY,
+            uint width, uint height, uint scaledWidth, uint scaledHeight)
+        {
+            BitmapTransform transform = new BitmapTransform();
+            BitmapBounds bounds = new BitmapBounds();
+            bounds.X = startPointX;
+            bounds.Y = startPointY;
+            bounds.Height = height;
+            bounds.Width = width;
+            transform.Bounds = bounds;
+            transform.ScaledWidth = scaledWidth;
+            transform.ScaledHeight = scaledHeight;
+            // Get the cropped pixels within the bounds of transform.
+            PixelDataProvider pix = await decoder.GetPixelDataAsync(
+                BitmapPixelFormat.Bgra8,
+                BitmapAlphaMode.Straight,
+                transform,
+                ExifOrientationMode.IgnoreExifOrientation,
+                ColorManagementMode.ColorManageToSRgb);
+            byte[] pixels = pix.DetachPixelData();
+            return pixels;
         }
     }
 }
